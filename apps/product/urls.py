109c5eff9323c16dabfb5813034094c032f147d5
Zoreyan <klovvergg@gmail.com>
2024-12-22 14:48:11 +0600

    git push force

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,31 +25,4 @@ urlpatterns = [
     path('category-create', category_create, name='category-create'),
     path('update_product_per_page', update_product_per_page, name='update_product_per_page'),
     path('update_category_per_page', update_category_per_page, name='update_category_per_page'),
-=======
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path('list/', list_, name='product-list'),
-    path('create/', create, name='product-create'),
-    path('at_income_create/', product_create, name='product-create-at-income'),
-    path('details/<int:pk>/', details, name='product-details'),
-    path('update/<int:pk>/', update, name='product-update'),
-    path('delete/<int:pk>/', delete, name='product-delete'),
-    path('sale/', sale, name='product-sale'),
-    path('income/', income, name='product-income'),
-    path('get-product/', get_product, name='get-product'),
-    path('create-sell-history/', create_sell_history, name='create-sell-history'),
-    path('create-income-history/', create_income_history, name='create-income-history'),
-    path('update-quantity/', update_quantity, name='update-quantity'),
-    path('find-product/', find_product, name='find-product'),
-    path('search-product/', search_product, name='search-product'),
-    path('category-list/', category_list, name='category-list'),
-    path('category-update/<int:pk>/', category_update, name='category-update'),
-    path('category-delete/<int:pk>/', category_delete, name='category-delete'),
-    path('category-create', category_create, name='category-create'),
-    path('update_product_per_page', update_product_per_page, name='update_product_per_page'),
-    path('update_category_per_page', update_category_per_page, name='update_category_per_page'),
->>>>>>> 0f5feb102b45a94eef3b618ce0c3188881e35ea1
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
