3a2b7a680d1e39d6f3cdebef592830167b204a65
Zoreyan <klovvergg@gmail.com>
2025-03-03 12:20:16 +0600

    DB changed

--- backend/settings.py
+++ backend/settings.py
@@ -84,29 +84,29 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 # MySQL
-# DATABASES = {
+DATABASES = {
 
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'cl17008_deer',
-#         'USER': 'cl17008_deer',
-#         'PASSWORD': 'deer',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         }
-#     }
-# }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cl17008_deer',
+        'USER': 'cl17008_deer',
+        'PASSWORD': 'deer',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
+    }
+}
 
 
 # SQLite
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
